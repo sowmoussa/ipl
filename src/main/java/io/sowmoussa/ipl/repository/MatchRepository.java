6c3348d10eb9f94deb563f433598f9a82f047747
Moussa SOW <moussa4.sow@gmail.com>
2021-05-05 18:11:47 +0200

    Pageable added

--- src/main/java/io/sowmoussa/ipl/repository/MatchRepository.java
+++ src/main/java/io/sowmoussa/ipl/repository/MatchRepository.java
@@ -1,10 +1,12 @@
 package io.sowmoussa.ipl.repository;
 
 import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import io.sowmoussa.ipl.model.Match;
 
 public interface MatchRepository extends CrudRepository<Match, Long> {
     
-    List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2);
+    List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
 }
