b07665cdf7f451580aa8d0f882df70ec22b80102
Moussa SOW <moussa4.sow@gmail.com>
2021-05-05 18:11:47 +0200

    Pageable added

--- src/main/java/io/sowmoussa/ipl/controller/TeamController.java
+++ src/main/java/io/sowmoussa/ipl/controller/TeamController.java
@@ -1,5 +1,7 @@
 package io.sowmoussa.ipl.controller;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +25,10 @@ public class TeamController {
     @GetMapping("/team/{teamName}")
     public Team getTeam(@PathVariable String teamName) {
         Team team = this.teamRepository.findByTeamName(teamName);
-        team.setMatches(this.matchRepository.getByTeam1OrTeam2OrderByDateDesc(teamName, teamName));
+
+        Pageable pageable = PageRequest.of(0, 4);
+
+        team.setMatches(this.matchRepository.getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, pageable));
 
         return team;
     }
