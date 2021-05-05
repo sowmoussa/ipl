195b8bcfd3443c26025dc16f26152e5b83e78ea6
Moussa SOW <moussa4.sow@gmail.com>
2021-05-05 18:20:36 +0200

    Moved Pageable model to repository instead of using it in my controller

--- src/main/java/io/sowmoussa/ipl/repository/MatchRepository.java
+++ src/main/java/io/sowmoussa/ipl/repository/MatchRepository.java
@@ -2,6 +2,7 @@ package io.sowmoussa.ipl.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import io.sowmoussa.ipl.model.Match;
@@ -9,4 +10,8 @@ import io.sowmoussa.ipl.model.Match;
 public interface MatchRepository extends CrudRepository<Match, Long> {
     
     List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
+
+    default List<Match> findLatestMatchesbyTeam(String teamName, int count) {
+        return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
+    }
 }
